5ec321c6e3173dda6ad4a901c3c7b4e2f874be4c
CKiilu <cndeti@gmail.com>
2016-03-07 15:55:30 +0300

    Listings activity

--- app/src/main/java/com/scurrae/chris/charged/Listings.java
+++ app/src/main/java/com/scurrae/chris/charged/Listings.java
@@ -1,7 +1,10 @@
 package com.scurrae.chris.charged;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by chris on 3/7/16.
@@ -11,5 +14,12 @@ public class Listings extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list);
+        TextView t = (TextView)findViewById(R.id.listtext);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(), Knowledge.class));
+            }
+        });
     }
 }
