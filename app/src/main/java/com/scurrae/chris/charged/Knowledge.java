6979abfa50d4ab815127bff1424326adbc578896
CKiilu <cndeti@gmail.com>
2016-03-07 16:10:46 +0300

    second what you know layout

--- app/src/main/java/com/scurrae/chris/charged/Knowledge.java
+++ app/src/main/java/com/scurrae/chris/charged/Knowledge.java
@@ -2,6 +2,7 @@ package com.scurrae.chris.charged;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
@@ -13,5 +14,10 @@ public class Knowledge extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.knowledge);
+        String[] categ = {"Fashion", "Entertainment", "Sports", "Travelling", "Shopping", "Science", "Technologies", "Finances" };
+        ArrayAdapter adapte = new ArrayAdapter<String>(this, R.layout.knowledge, categ);
+
+        final ListView listView = (ListView) findViewById(R.id.lsview);
+        listView.setAdapter(adapte);
     }
 }
