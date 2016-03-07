6a6f3ecb9b6eb0596d4ecb0d003faefb4a8f3456
CKiilu <cndeti@gmail.com>
2016-03-07 15:53:03 +0300

    Login activity

--- app/src/main/java/com/scurrae/chris/charged/Login.java
+++ app/src/main/java/com/scurrae/chris/charged/Login.java
@@ -1,8 +1,11 @@
 package com.scurrae.chris.charged;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends Activity {
 
@@ -10,5 +13,12 @@ public class Login extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        TextView t = (TextView)findViewById(R.id.login);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(), Listings.class));
+            }
+        });
     }
 }
